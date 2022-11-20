8115cea98ae7cb65756013a6057be111c6562714
Milenin <307455@niuitmo.ru>
2022-11-21 00:32:29 +0300

    serv_send_211120220032

--- bot/bot.php
+++ bot/bot.php
@@ -5,27 +5,29 @@ function bot_sendMessage($user_id, $data)
     $msg = "Привет, {$user_id}!";
     //$curr_data = stateById($user_id);
     vkApi_messagesSend($user_id, $msg);
-    vkApi_messagesSend($user_id, $msg.'---');
+    vkApi_messagesSend($user_id, stateById($user_id));
 }
 
 function stateById($user_id)
 {
     $database = new Database();
     $db = $database->getConnection();
+    vkApi_messagesSend($user_id, $db);
 
     $item = new User($db);
 
     $item->id = $user_id;
     $item->getSingleUser();
+    vkApi_messagesSend($user_id, $item->stateId);
     if ($item->stateId != null) {
         // create array
 
     } else {
-        echo "Creating%n";
+        vkApi_messagesSend($user_id, "Creating");
         $item->stateId = 0;
         $item->stateArgs = EMPTY_JSON_STATE;
         $item->bills = EMPTY_JSON_IDS_ARRAY;
-        echo $item->createUser();
+        vkApi_messagesSend($user_id, $item->createUser());
     }
     $usr_arr = array(
         "id" => $item->id,
