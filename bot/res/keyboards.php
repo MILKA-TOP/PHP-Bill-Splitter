2f4f0ba73b372b71356a4538b38aa5b935965c9b
Milenin <307455@niuitmo.ru>
2022-11-25 13:37:17 +0300

    serv_send_251120221337

--- bot/res/keyboards.php
+++ bot/res/keyboards.php
@@ -1,16 +1,16 @@
 <?php
 const MAIN_KEYBOARD = [
     "one_time" => false,
-    "buttons" => [[[
-        "action" => [
+    "buttons" => [[
+        ["action" => [
             "type" => "text",
             "label" => CREATE_BILL_BUTTON_TEXT],
-        "color" => "positive"],
-        ["action" => [
+            "color" => "positive"]],
+        [["action" => [
             "type" => "text",
             "label" => SHOW_BILLS_BUTTON_TEXT],
-            "color" => "primary"],
-        ["action" => [
+            "color" => "primary"]],
+        [["action" => [
             "type" => "text",
             "label" => HELP_TEXT],
             "color" => "secondary"]]]];
