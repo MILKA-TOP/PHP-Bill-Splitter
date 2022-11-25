4fe477ad5a356e4bd01faf336bd14f51ec244930
Milenin <307455@niuitmo.ru>
2022-11-25 14:11:45 +0300

    serv_send_251120221411

--- bot/actions/MainScreen.php
+++ bot/actions/MainScreen.php
@@ -2,21 +2,34 @@
 
 function mainStateAction($user_id, $data, $db)
 {
-    if (isset($data["payload"])) {
-        vkApi_messagesSend($user_id, $data["payload"]);
-    } else {
-        vkApi_messagesSend($user_id, $data);
+    if (checkStartPayload($data)) {
+        vkApi_messagesSend($user_id, START_MESSAGE, MAIN_KEYBOARD);
+        return;
     }
-    switch ($data["message"]["text"]) {
-        case CREATE_BILL_BUTTON_TEXT:
-            startCreateUserBills($user_id, $db);
-            break;
-        case SHOW_BILLS_BUTTON_TEXT:
-            showUserBills($user_id, $db);
-            break;
-        default:
-            vkApi_messagesSend($user_id, START_MESSAGE, MAIN_KEYBOARD);
+
+    if (isset($data["message"]["payload"]))
+        switch ($data["message"]["text"]) {
+            case CREATE_BILL_BUTTON_TEXT:
+                startCreateUserBills($user_id, $db);
+                break;
+            case SHOW_BILLS_BUTTON_TEXT:
+                showUserBills($user_id, $db);
+                break;
+            case HELP_TEXT:
+                vkApi_messagesSend($user_id, START_MESSAGE, MAIN_KEYBOARD);
+                break;
+            default:
+                vkApi_messagesSend($user_id, ERROR_MAIN_MESSAGE, MAIN_KEYBOARD);
+        }
+}
+
+function checkStartPayload($data)
+{
+    if (isset($data["message"]["payload"])) {
+        $start_json_array = json_decode($data["message"]["payload"]);
+        return isset($start_json_array["command"]) && $start_json_array["command"] == "start";
     }
+    return false;
 }
 
 function showUserBills($user_id, $db)
---
+++ bot/config/payloads.php
@@ -0,0 +1,5 @@
+<?php
+
+const CREATE_BILL_BUTTON_TEXT = "Создать счёт";
+const SHOW_BILLS_BUTTON_TEXT = "Показать доступные счета";
+const HELP_TEXT = "Помощь";
