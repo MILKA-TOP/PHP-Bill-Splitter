35b21123c3f6426aecb88748ed9e3bb7e3f7f90d
Milenin <307455@niuitmo.ru>
2022-11-25 13:49:31 +0300

    serv_send_251120221342

--- bot/actions/MainScreen.php
+++ bot/actions/MainScreen.php
@@ -2,13 +2,27 @@
 
 function mainStateAction($user_id, $data, $db)
 {
-    vkApi_messagesSend($user_id, $data["payload"]);
+    if (isset($data["payload"])) {
+        vkApi_messagesSend($user_id, $data["payload"]);
+    }
     switch ($data["message"]["text"]) {
         case CREATE_BILL_BUTTON_TEXT:
+            startCreateUserBills($user_id, $db);
             break;
         case SHOW_BILLS_BUTTON_TEXT:
+            showUserBills($user_id, $db);
             break;
         default:
             vkApi_messagesSend($user_id, START_MESSAGE, MAIN_KEYBOARD);
     }
+}
+
+function showUserBills($user_id, $db)
+{
+    vkApi_messagesSend($user_id, DEVELOP_MESSAGE, MAIN_KEYBOARD);
+}
+
+function startCreateUserBills($user_id, $db)
+{
+    vkApi_messagesSend($user_id, DEVELOP_MESSAGE, MAIN_KEYBOARD);
 }
