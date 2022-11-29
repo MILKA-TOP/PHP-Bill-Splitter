2104367e3887a8947f7df7bbfacb3b5968ac007c
Milenin <307455@niuitmo.ru>
2022-11-29 18:46:29 +0300

    serv_send_291120221846

--- bot/config/json.php
+++ bot/config/json.php
@@ -8,12 +8,12 @@ define("EMPTY_JSON_STATE", json_encode($empty_json));
 
 function setNameStateJsonArgument($name)
 {
-    return json_encode(array(NAME_STATE_ARG => $name));
+    return json_encode(array(NAME_STATE_ARG => $name), JSON_UNESCAPED_UNICODE);
 }
 
 function setPasswordFieldToJson($input_json, $password)
 {
     $current_json_array = json_decode($input_json, true);
     $current_json_array[PASSWORD_STATE_ARG] = $password;
-    return json_encode($current_json_array);
+    return json_encode($current_json_array, JSON_UNESCAPED_UNICODE);
 }
