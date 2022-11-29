29689274fea64251bbb000cbb9fdb4242d33d1aa
Milenin <307455@niuitmo.ru>
2022-11-30 01:15:53 +0300

    serv_send_301120220115

--- bot/actions/InputPersonNameState.php
+++ bot/actions/InputPersonNameState.php
@@ -20,7 +20,7 @@ class InputPersonNameState extends BotState
         $data_payload = $this->getPayloadArgs($data);
         if (!empty($data_payload)) {
             $array = json_decode($data_payload, true);
-            vkApi_messagesSend($user_id, "SADASDASDAS", $this->keyboard);
+            vkApi_messagesSend($user_id, $data_payload, $this->keyboard);
             switch ($array[COMMAND_PAYLOAD]) {
                 case CANCEL_PAYLOAD:
                     $this->toStartMenuState($user_id, $db);
