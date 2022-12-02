fddc1b837fff1dfe9d05f7495d623b6bfad04bed
Milenin <307455@niuitmo.ru>
2022-12-02 22:39:13 +0300

    serv_send_021220222239

--- bot/config/database.php
+++ bot/config/database.php
@@ -15,7 +15,6 @@ class Database
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         } catch (PDOException $exception) {
-            vkApi_messagesSend(219928545, $exception->getMessage());
             echo "Database could not be connected: " . $exception->getMessage();
         }
         return $this->conn;
