b8fb85af2fff8974879a12114e53b1ce57dd0c40
Milenin <307455@niuitmo.ru>
2022-12-03 02:01:55 +0300

    serv_send_031220220201

--- bot/actions/MainBillState.php
+++ bot/actions/MainBillState.php
@@ -90,9 +90,12 @@ class MainBillState extends BotState
 
             foreach ($fields_ids as $currFieldId) {
                 $field->id = $currFieldId;
-                $field->getField();
+                $field->getSingleField();
 
-                $item_list_string = $item_list_string . sprintf(SINGLE_FIELD_FOR_BILL, $field->name, $field->price);
+                $item_list_string = $item_list_string . sprintf(
+                        $format_field_line,
+                        $field->name,
+                        $field->price / count($single_bill->persons));
             }
         }
 
